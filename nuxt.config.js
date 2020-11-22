72b2c5bd24609b50f1ecbcfaea6fa492fb159403
Daniel Priest <galanest@ya.ru>
2020-11-22 16:55:10 +0300

    Added SCSS resources global import

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,8 +16,15 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
+  
+  // Global Style Imports
+  styleResources: {
+    scss: [
+      '@/style/vars/_colors.scss',
+      '@/style/vars/_sizes.scss'
+    ]
+  },
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
@@ -35,7 +42,8 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/style-resources'
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
