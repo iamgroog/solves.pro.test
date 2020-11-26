962c4510887238d6157cb14c4e66ae9f5ef41b66
Daniel Priest <galanest@ya.ru>
2020-11-26 13:42:46 +0300

    Changed rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,19 @@ module.exports = {
     node: true
   },
   extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
+    "@nuxtjs/eslint-config-typescript",
+    "plugin:nuxt/recommended"
   ],
   plugins: [
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    // eslint-disable-next-line quotes
+    "space-before-blocks": 0,
+    quotes: ["error", "double"],
+    "space-before-function-paren": 0,
+    semi: 0,
+    "eol-last": 0,
+    "vue/name-property-casing": ["error", "kebab-case"]
+  }
 }
