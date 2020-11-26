8dd17230cc6f5a1dd7129cd7477e6ca17c04519b
Daniel Priest <galanest@ya.ru>
2020-11-26 13:44:18 +0300

    Add components

---
+++ components/layout.blocks/TheHeader.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+  div.the-header
+    div.the-header__title Конструктор
+    div.the-header__controls
+      div.the-header__toggle {{changeModeText}}
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import { mapGetters } from "vuex";
+
+export default Vue.extend({
+  name: "the-header",
+  computed: {
+    ...mapGetters(["isPreviw", "isEdit"]),
+    changeModeText(){
+      if (this.isPreviw) {
+        return "Редактировать"
+      }
+      return "Предпросмотр"
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+  .the-header{
+    padding: 5px;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    color: $color-secondary;
+    background-color: $color-main;
+
+    &__title{
+      font-size: $size-title;
+      color: $color-light;
+    }
+
+    // &__controls{
+
+    // }
+
+    // &__toggle{
+
+    // }
+  }
+</style>
---
+++ components/view.blocks/TheConstructor.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div.the-constructor
+    template
+      div.the-constructor__empty
+        h1 Пока ничего нет
+        div Создайте свой уникальный шаблон из блоков
+        div Добавить новый блок
+    template
+</template>
+
+<script lang="ts">
+  import Vue from "vue"
+  export default Vue.extend({
+    name: "the-constructor"
+  })
+</script>
+
+<style lang="scss">
+  .the-constructor{
+
+  }
+</style>
