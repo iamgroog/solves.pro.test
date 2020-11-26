185ef95c8f5b0b9099d64b451abf623ffc2e6437
Daniel Priest <galanest@ya.ru>
2020-11-26 14:07:25 +0300

    Update mode changing

--- components/layout.blocks/TheHeader.vue
+++ components/layout.blocks/TheHeader.vue
@@ -2,25 +2,46 @@
   div.the-header
     div.the-header__title Конструктор
     div.the-header__controls
-      div.the-header__toggle {{changeModeText}}
+      div.the-header__toggle(
+        @click="onChangeMode"
+      ) {{changeModeText}}
 </template>
 
 <script lang="ts">
-import Vue from "vue"
-import { mapGetters } from "vuex";
+  import Vue from "vue"
+  import { mapMutations, mapState } from "vuex";
 
-export default Vue.extend({
-  name: "the-header",
-  computed: {
-    ...mapGetters(["isPreviw", "isEdit"]),
-    changeModeText(){
-      if (this.isPreviw) {
-        return "Редактировать"
+  export default Vue.extend({
+    name: "the-header",
+    computed: {
+      ...mapState(["mode"]),
+      /**
+       * Получить текст кнопки управления режимом конструктора
+       */
+      changeModeText(): string {
+        switch (this.mode) {
+          case "preview": return "Предпросмотр";
+          case "edit":
+          default: return "Редактировать";
+        }
       }
-      return "Предпросмотр"
+    },
+    methods: {
+      ...mapMutations({
+        changeMode: "CHANGE_MODE"
+      }),
+      /**
+       * Установить режим конструктора
+       */
+      onChangeMode(){
+        switch (this.mode) {
+          case "edit": this.changeMode("preview"); break;
+          case "preview":
+          default: this.changeMode("edit");
+        }
+      }
     }
-  }
-})
+  })
 </script>
 
 <style lang="scss">
